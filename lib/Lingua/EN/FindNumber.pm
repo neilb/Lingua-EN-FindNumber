43f069e6f9ff2c9347cde183872a201705ef3429
Tony Bowden <tony@tmtm.com>
2004-10-24 07:12:39 -0800

    import Lingua-EN-FindNumber 1.1 from CPAN
    git-cpan-module:   Lingua-EN-FindNumber
    git-cpan-version:  1.1
    git-cpan-authorid: TMTM
    git-cpan-file:     authors/id/T/TM/TMTM/Lingua-EN-FindNumber-1.1.tar.gz

--- lib/Lingua/EN/FindNumber.pm
+++ lib/Lingua/EN/FindNumber.pm
@@ -1,35 +1,37 @@
 package Lingua::EN::FindNumber;
-use Lingua::EN::Words2Nums;
+
 use 5.006;
+our $VERSION = '1.1';
+
 use strict;
 use warnings;
 
-require Exporter;
-
-our @ISA = qw(Exporter);
+use base 'Exporter';
 our @EXPORT = qw( extract_numbers $number_re numify );
-our $VERSION = '1.0';
+
+use Lingua::EN::Words2Nums;
 
 # This is from Lingua::EN::Words2Nums, after being thrown through
 # Regex::PreSuf
-my $numbers = qr/((?:b(?:akers?dozen|illi(?:ard|on))|centillion|d(?:ecilli(?:ard|on)|ozen|u(?:o(?:decilli(?:ard|on)|vigintillion)|vigintillion))|e(?:ight(?:een|ieth|[yh])?|leven(?:ty(?:first|one))?|s)|f(?:i(?:ft(?:een|ieth|[yh])|rst|ve)|o(?:rt(?:ieth|y)|ur(?:t(?:ieth|[yh]))?))|g(?:oogol(?:plex)?|ross)|hundred|mi(?:l(?:ion|li(?:ard|on))|nus)|n(?:aught|egative|in(?:et(?:ieth|y)|t(?:een|[yh])|e)|o(?:nilli(?:ard|on)|ught|vem(?:dec|vigint)illion))|o(?:ct(?:illi(?:ard|on)|o(?:dec|vigint)illion)|ne)|qu(?:a(?:drilli(?:ard|on)|ttuor(?:decilli(?:ard|on)|vigintillion))|in(?:decilli(?:ard|on)|tilli(?:ard|on)|vigintillion))|s(?:core|e(?:cond|pt(?:en(?:dec|vigint)illion|illi(?:ard|on))|ven(?:t(?:ieth|y))?|x(?:decillion|tilli(?:ard|on)|vigintillion))|ix(?:t(?:ieth|y))?)|t(?:ee?n|h(?:ir(?:t(?:een|ieth|y)|d)|ousand|ree)|r(?:e(?:decilli(?:ard|on)|vigintillion)|i(?:gintillion|lli(?:ard|on)))|w(?:e(?:l(?:fth|ve)|nt(?:ieth|y))|o)|h)|un(?:decilli(?:ard|on)|vigintillion)|vigintillion|zero|s))/i;
+my $numbers =
+	qr/((?:b(?:akers?dozen|illi(?:ard|on))|centillion|d(?:ecilli(?:ard|on)|ozen|u(?:o(?:decilli(?:ard|on)|vigintillion)|vigintillion))|e(?:ight(?:een|ieth|[yh])?|leven(?:ty(?:first|one))?|s)|f(?:i(?:ft(?:een|ieth|[yh])|rst|ve)|o(?:rt(?:ieth|y)|ur(?:t(?:ieth|[yh]))?))|g(?:oogol(?:plex)?|ross)|hundred|mi(?:l(?:ion|li(?:ard|on))|nus)|n(?:aught|egative|in(?:et(?:ieth|y)|t(?:een|[yh])|e)|o(?:nilli(?:ard|on)|ught|vem(?:dec|vigint)illion))|o(?:ct(?:illi(?:ard|on)|o(?:dec|vigint)illion)|ne)|qu(?:a(?:drilli(?:ard|on)|ttuor(?:decilli(?:ard|on)|vigintillion))|in(?:decilli(?:ard|on)|tilli(?:ard|on)|vigintillion))|s(?:core|e(?:cond|pt(?:en(?:dec|vigint)illion|illi(?:ard|on))|ven(?:t(?:ieth|y))?|x(?:decillion|tilli(?:ard|on)|vigintillion))|ix(?:t(?:ieth|y))?)|t(?:ee?n|h(?:ir(?:t(?:een|ieth|y)|d)|ousand|ree)|r(?:e(?:decilli(?:ard|on)|vigintillion)|i(?:gintillion|lli(?:ard|on)))|w(?:e(?:l(?:fth|ve)|nt(?:ieth|y))|o)|h)|un(?:decilli(?:ard|on)|vigintillion)|vigintillion|zero|s))/i;
 
-my $ok_words = qr/\b(and|a|of)\b/;
+my $ok_words  = qr/\b(and|a|of)\b/;
 my $ok_things = qr/[^A-Za-z0-9.]/;
 our $number_re = qr/\b(($numbers($ok_words|$ok_things)*)+)\b/i;
 
 sub extract_numbers {
-    my $text = shift;
-    my @numbers;
-    push @numbers, $1 while $text =~ /$number_re/g;
-    s/\s+$// for @numbers;
-    return @numbers;
+	my $text = shift;
+	my @numbers;
+	push @numbers, $1 while $text =~ /$number_re/g;
+	s/\s+$// for @numbers;
+	return @numbers;
 }
 
 sub numify {
-    my $text = shift;
-    $text =~ s/$number_re/words2nums($1). ($1 =~ m{(\s+)$} ? $1 :"")/eg;
-    return $text;
+	my $text = shift;
+	$text =~ s/$number_re/words2nums($1). ($1 =~ m{(\s+)$} ? $1 :"")/eg;
+	return $text;
 }
 
 1;
@@ -70,13 +72,20 @@ why not buy the book?
 
 This module works rather well in conjunction with
 L<Lingua::EN::Words2Nums>, which is a very cool module anyway. (And I
-stole some of this module's code from it. Thanks, Joey!) It also should
+stole some of this module's code from it. Thanks, Joey!) It may also
 be involved with L<Lingua::EN::NamedEntity> in the future, so check that
 one out too.
 
 =head1 AUTHOR
 
-Simon Cozens, E<lt>simon@kasei.comE<gt>
+Original author: Simon Cozens
+
+Current maintainer: Tony Bowden
+
+=head1 BUGS and QUERIES
+
+Please direct all correspondence regarding this module to:
+  bug-Lingua-EN-Number@rt.cpan.org
 
 =head1 COPYRIGHT AND LICENSE
 
